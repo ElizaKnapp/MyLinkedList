28ef21ae66565c7536df036e85d99acb48dd365d
ElizaKnapp <eknapp20@stuy.edu>
2021-01-06 12:15:41 -0500

    get and set methods done. the sets return the replaced value in string form

--- Node.java
+++ Node.java
@@ -1,6 +1,36 @@
 public class Node {
-  public Node(String value){/*create a constructor*/}
   private String data;
-  private Node next,prev;
+  private Node next, prev;
+
+  public Node(String value){
+    data = value;
+  }
+
+  public String getData() {
+    return data;
+  }
+  public String setData(String value) {
+    temp = data;
+    data = value;
+    return temp;
+  }
+
+  public String getNext() {
+    return next;
+  }
+  public String setNext(String value) { //returns the removed value
+    temp = next;
+    next = value;
+    return temp;
+  }
+
+  public String getPrev() {
+    return prev;
+  }
+  public String setPrev(String value) { //returns the removed value
+    temp = prev;
+    prev = value;
+    return temp;
+  }
   //write get/set methods for all three instance variables.
 }
