27581b74ed27262a5cdf9a79923f6d52e1d1dcb0
ElizaKnapp <eknapp20@stuy.edu>
2021-01-06 21:48:12 -0500

    add done

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -26,7 +26,32 @@ public class MyLinkedList {
  }
 
  public void add(int index, String value) {
-
+   Node toAdd = new Node(value);
+   if (size == 0) { //empty list
+     start = toAdd;
+     end = toAdd;
+   }
+   else if (index == 0) {  //head node switch
+     Node holder = new Node(start.getData()); //aka the og start
+     start.setPrev(toAdd); //original start now points back to toAdd
+     start = toAdd; //start becomes toAdd
+     start.getNext(holder);  //next to start becomes old start
+   }
+   else if (index == size) { //tail node switch
+     Node holder = end; //aka the og end
+     end.setNext(toAdd);
+     end = toAdd;
+     end.setPrev(holder);
+   }
+   else {  //all other nodes
+     Node theOneMovedRight = findNode(index);
+     theOneMovedRight.setPrev(toAdd); //so now it has a reference to the new added ones
+     Node theOneMovedLeft = findNode(index - 1);
+     theOneMovedLeft.setNext(toAdd);
+     toAdd.setNext(theOneMovedRight);
+     toAdd.setPrev(theOneMovedLeft);
+   }
+   size++;
  }
 
  public String get(int index);
