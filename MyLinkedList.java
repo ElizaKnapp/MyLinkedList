07e5b31ca7279d35a6109df508c0136a7fa14de8
ElizaKnapp <eknapp20@stuy.edu>
2021-01-06 21:03:05 -0500

    size done

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,10 +1,19 @@
 public class MyLinkedList {
  private int size;
  private Node start,end;
- public MyLinkedList(){/*create a constructor*/}
- public int size();
+
+ public MyLinkedList(){
+   size = 0;
+   start = Node()
+   end = 0;
+ }
+
+ public int size() {
+   return size;
+ }
+
  public boolean add(String value);
- public boolean add(int index, String value);
+ public void add(int index, String value);
  public String get(int index);
  public String set(int index, String value);
  public String toString();
